5767f2aaf1e1c32deea1ab2c075226aaa6762f75
addas3 <addas3code@gmail.com>
2023-09-23 12:18:11 +0000

    Set STATIC_ROOT and created 'static' directory

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -22,6 +22,8 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
